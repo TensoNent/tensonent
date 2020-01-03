09a99b03d572bd5ea2b47e5536f751dec3f817c8
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2020-01-03 12:40:55 +0330

    Defining kronecker function and adding its body

--- tensonent/tensor_algebra/kronecker.py
+++ tensonent/tensor_algebra/kronecker.py
@@ -5,3 +5,19 @@
 """
 
 from numpy import kron
+
+
+def kronecker(p_1, p_2, *args):
+    """
+    Kronecker Product for series of tensors, matrices , vectors or scalars
+
+    :param p_1: ndarray, iterable, int, float
+    :param p_2: ndarray, iterable, int, float
+    :param args: optional or ndarray, iterable, int, float
+    :return: ndarray
+    """
+    res = kron(p_1, p_2)
+    if args:
+        for item in args:
+            res = kron(res, item)
+    return res
